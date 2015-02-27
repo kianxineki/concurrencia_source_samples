60e027c1e334139eb5b10dc136dd678360358839
KiAn Xineki <galerajimenez@gmail.com>
2015-02-27 09:20:31 +0100

    Update counter.py
    Añadido un lock para que el resultado final sea correcto, igualmente, la operación es ineficiente por el GIL, a mas threads peor rendimiento tendrá (en este caso en concreto)

--- counter.py
+++ counter.py
@@ -1,37 +1,42 @@
 import threading
-import time
 
 MAX_COUNT = 10000000
 THREADS = 2
 
-class Counter(threading.Thread):
-	counter = 0
 
-	def __init__(self, threadID, counts):
-		threading.Thread.__init__(self)
-		self.threadID = threadID
-		self.counts = counts
-	def run(self):
-		print("Starting " + repr(self.threadID))
-		for i in range(self.counts):
-			Counter.counter += 1
-		print("End {} counter: {}".format(self.threadID, Counter.counter))
+class Counter(threading.Thread):
+    counter = 0
+    lock = threading.Lock()
+
+    def __init__(self, threadID, counts):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.counts = counts
+
+    def incremental(self):
+        with self.lock:
+            Counter.counter += 1
+
+    def run(self):
+        print("Starting " + repr(self.threadID))
+        for i in range(self.counts):
+            self.incremental()
+        print("End {} counter: {}".format(self.threadID, Counter.counter))
 
 threads = []
 
 for i in range(THREADS):
-	# Create new threads
-	t = Counter(i, int(MAX_COUNT/THREADS))
-	threads.append(t)
+    # Create new threads
+    t = Counter(i, int(MAX_COUNT/THREADS))
+    threads.append(t)
 
 for t in threads:
-	# Start threads
-	t.start()
+    # Start threads
+    t.start()
 
 
 # Wait for all threads to complete
 for t in threads:
-	t.join()
+    t.join()
 
 print("Counter value: {} Expected: {}".format(Counter.counter, MAX_COUNT))
-
